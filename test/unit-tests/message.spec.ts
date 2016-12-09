097f270721fc2e7169e835cea1a546278b3b012d
James Richford <jamesrichford@googlemail.com>
2016-12-09 22:22:55 +0000

    message tests

---
+++ test/unit-tests/message.spec.ts
@@ -0,0 +1,31 @@
+import { Expect, TestCase, Test } from "alsatian";
+import ExtendoError from "../../src/extendo-error";
+
+export class MessageTests {
+
+    @TestCase(null)
+    @TestCase(undefined)
+    @TestCase("")
+    @TestCase("something went wrong")
+    @TestCase("another awful thing happened")
+    @Test("message set in constructor")
+    public messageSetInConstructor(expectedMessage: string) {
+        const extendoError = new ExtendoError(expectedMessage);
+
+        Expect(extendoError.message).toBe(expectedMessage);
+    }
+
+    @TestCase(null)
+    @TestCase(undefined)
+    @TestCase("")
+    @TestCase("something went wrong")
+    @TestCase("another awful thing happened")
+    @Test("message set in constructor")
+    public messageCanBeSetAfterConstructor(expectedMessage: string) {
+        const extendoError = new ExtendoError("original message.");
+
+        extendoError.message = expectedMessage;
+
+        Expect(extendoError.message).toBe(expectedMessage);
+    }
+}
